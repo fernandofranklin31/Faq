8a24428af155f1e9d1d2b989d23b01283029d733
Fernando Franklin <fernandoararipe1@gmail.com>
2023-12-20 18:50:36 -0300

    ...

--- index.js
+++ index.js
@@ -12,107 +12,21 @@ var imgThirdPlus = document.getElementById('imgThirdPlus');
 var imgFourPlus = document.getElementById('imgFourPlus');
 var btns = document.querySelectorAll('button');
 var parags = document.querySelectorAll('p');
-var imgs = document.querySelectorAll('.imgPlusButtons');
-var flagBtn = true;
+var plusImgs = document.querySelectorAll('.imgPlusButtons');
 
 
-flagBtn ? btns.forEach(x=>x.addEventListener('click', clickPlusBtn)) : btns.forEach(x=>x.addEventListener('click', clickMinusBtn))
+btns.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        var isHidden = parags[index].classList.contains('hidden');
 
-
-function clickPlusBtn() {
-    debugger;
-    switch(this) {
-        case firstBtn: 
-        firstParag.classList.remove('hidden');
-        imgFirstPlus.setAttribute('src', '/assets/images/icon-minus.svg');
-        flagBtn = false;
-        break;
-
-        case secondBtn: 
-        secondParag.classList.remove('hidden');
-        imgSecondPlus.setAttribute('src', '/assets/images/icon-minus.svg');
-        
-
-        break;
-
-        case thirdBtn: 
-        thirdParag.classList.remove('hidden');
-        imgThirdPlus.setAttribute('src', '/assets/images/icon-minus.svg');
-       
-
-        break;
-
-        case fourBtn: 
-        fourParag.classList.remove('hidden');
-        imgFourPlus.setAttribute('src', '/assets/images/icon-minus.svg');
-       
-
-        break;
-
-        default:
-            return;
-    }};
-    
-
-    function clickMinusBtn() {
-        switch(this) {
-            case firstBtn: 
-            firstParag.classList.add('hidden');
-            imgFirstPlus.setAttribute('src', '/assets/images/icon-plus.svg');
-            flagBtn=true;
-            break;
-    
-            case secondBtn: 
-            secondParag.classList.add('hidden');
-            imgSecondPlus.setAttribute('src', '/assets/images/icon-plus.svg');
-            flagBtn=true;
-            break;
-    
-            case thirdBtn: 
-            thirdParag.classList.add('hidden');
-            imgThirdPlus.setAttribute('src', '/assets/images/icon-plus.svg');
-            flagBtn=true;
-            break;
-    
-            case fourBtn: 
-            fourParag.classList.add('hidden');
-            imgFourPlus.setAttribute('src', '/assets/images/icon-plus.svg');
-            flagBtn=true;
-            break;
-    
-            default:
-                return;
-        }};
-
-
-        
- 
-//     firstParag.classList.remove('hidden');
-//     secondParag.classList.remove('hidden');
-//     thirdParag.classList.remove('hidden');
-//     fourParag.classList.remove('hidden');
-//     imgFirstPlus.setAttribute('src', '/assets/images/icon-minus.svg');
-//     imgSecondPlus.setAttribute('src', '/assets/images/icon-minus.svg');
-//     imgThirdPlus.setAttribute('src', '/assets/images/icon-minus.svg');
-//     imgFourPlus.setAttribute('src', '/assets/images/icon-minus.svg');
-// }
-
-// firstBtn.addEventListener('click', () => {;
-//          firstParag.classList.remove('hidden');
-//          imgFirstPlus.setAttribute('src', '/assets/images/icon-minus.svg');   
-//     });
-
-
-
-    
-
-
-
-
-
-
-
-// firstBtn.addEventListener('click', () => {
-//     plusButtonClicked();
-// })
+        if (isHidden) {
+            parags[index].classList.remove('hidden');
+            plusImgs[index].setAttribute('src', '/assets/images/icon-minus.svg');
+        }
+        else {
+            parags[index].classList.add('hidden');
+            plusImgs[index].setAttribute('src', '/assets/images/icon-plus.svg');
+        }
+    });
+});
 
